99ac5a303b383d4f01f76c0bcfbb30b04113272f
Evgeniiy <ewwwgeny@yandex.ru>
2019-02-09 20:11:46 +0300

    brain-games now run radom game

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,10 +1,26 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+// import readlineSync from 'readline-sync';
+import runCalcGame from '../games/calc';
+import runEvenGame from '../games/even';
+import runGcdGame from '../games/gcd';
+import runPrimeGame from '../games/prime';
+import runProgressionGame from '../games/progression';
+import getRandomNumber from '../utils';
 
-const greetUser = () => {
+const games = [
+  () => runCalcGame(),
+  () => runEvenGame(),
+  () => runGcdGame(),
+  () => runPrimeGame(),
+  () => runProgressionGame(),
+];
+
+games[getRandomNumber(0, games.length - 1)]();
+
+/* const greetUser = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
 console.log('Welcome to the Brain Games!');
-greetUser();
+greetUser(); */
